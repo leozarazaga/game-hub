e583ba123d0c2d81f11080172130e3e95f84f2f0
Leo Zarazaga <lza1313@outlook.com>
2025-07-15 14:48:07 +0200

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,10 +3,15 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import type { Genre } from "../hooks/useGenres";
 
-const GameGrid = () => {
-    const { data, error, isLoading } = useGames();
-    const skeletons = [1, 2, 3, 4, 5, 6];
+interface GameGridProps{
+    selectedGenre: Genre | null;
+}
+
+const GameGrid: React.FC<GameGridProps> = ({selectedGenre}) => {
+    const { data, error, isLoading } = useGames(selectedGenre);
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     return (
         <>
