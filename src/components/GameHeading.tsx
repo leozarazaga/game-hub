cd0a3df8fc832f3d4f211bafda4a7d6afd82ec19
Leo Zarazaga <lza1313@outlook.com>
2025-07-15 16:46:36 +0200

    Added a dynamic page heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,14 @@
+import { Heading } from "@chakra-ui/react";
+import type { GameQuery } from "../App";
+
+interface GameHeadingProps {
+    gameQuery: GameQuery;
+}
+
+const GameHeading: React.FC<GameHeadingProps> = ({ gameQuery }) => {
+    const heading = `${gameQuery.platform?.name || ""} ${gameQuery.genre?.name || ""} Games`;
+
+    return <Heading as={"h1"} marginY={5} fontSize="5xl">{heading}</Heading>;
+};
+
+export default GameHeading;
