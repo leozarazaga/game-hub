f2ea229d32ec56cc23472441c0c254292fb3c11d
Leo Zarazaga <lza1313@outlook.com>
2025-08-06 17:12:33 +0200

    Updated game logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,14 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
-import ColorModeSwitch from "./ColorModeSwitch";
-import SearchInput from "./SearchInput";
 import { Link } from "react-router";
+import ColorModeSwitch from "./ColorModeSwitch";
+import logo from "../assets/logo-pink-turquoise.png";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
     return (
         <HStack padding="10px">
             <Link to="/">
-                <Image src={logo} boxSize="60px" objectFit="cover" />
+                <Image src={logo} boxSize="60px" objectFit="cover" width="16" height="auto" borderRadius="10" />
             </Link>
             <SearchInput />
             <ColorModeSwitch />
